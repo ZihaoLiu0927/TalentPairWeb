1266967e207bd5a9f430f8f7c9d2d219965093af
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2023-03-25 02:19:47 -0400

    abstract entry point

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -1,4 +1,7 @@
-import 'landing/LandingIndex';
-import 'login/LoginIndex';
+import {mount as landingMount} from 'landing/LandingIndex';
+import { mount as loginMount } from 'login/LoginIndex';
 
 console.log('Container!');
+
+loginMount(document.querySelector('.login'));
+landingMount(document.querySelector('.landing'))
