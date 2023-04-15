f7b18e2eae18f343ac99b7ac8f363a3f9661bae4
ZihaoLiu0927 <Liuxiaohao0927@outlook.com>
2023-04-15 13:05:53 -0400

    landing layout change

--- landing/src/components/SectionAbout.js
+++ landing/src/components/SectionAbout.js
@@ -12,16 +12,17 @@ export default function SectionAbout() {
                 </div>
               </div>
               <div className="flex-1-of-3">
-                <div className="section-about-text u-center-text">
-                    <h1 className="heading-primary--main u-margin-top-huge">TalentPair</h1>
-                    <h1 className="heading-primary--sub u-margin-left-small u-margin-right-small u-margin-bottom-big"> Matching Making for Tech Startups </h1>
-                    <h1 className="heading_secondary u-margin-left-huge u-margin-right-huge">TalentPair aims to match tech job seekers with startup recruiters efficiently and affordably.</h1>
-                </div>
+                <div className='section-about--right-panel'>
+                  <div className="section-about--text u-center-text">
+                      <h1 className="heading-primary--main u-margin-top-huge">TalentPair</h1>
+                      <h1 className="heading-primary--sub u-margin-left-small u-margin-right-small u-margin-bottom-huge"> Matching Making for Tech Startups </h1>
+                      <h1 className="heading_secondary u-margin-left-medium u-margin-right-medium u-margin-top-huge">TalentPair aims to match tech job seekers with startup recruiters efficiently and affordably.</h1>
+                  </div>
 
-                <div>
-                    <img src={imgabout2} alt='Fig about' className="section-about--fig-img-2"/>
+                  <div className='section-about--contain-img-2'>
+                      <img src={imgabout2} alt='Fig about' className="section-about--fig-img-2"/>
+                  </div>
                 </div>
-
               </div>
             </div>
         </div>
--- landing/src/components/SectionIntro.js
+++ landing/src/components/SectionIntro.js
@@ -11,7 +11,7 @@ export default function SectionIntro() {
               <div className="flex-2-of-3">
                 <div className="section-intro-text">
                     <h1 className="heading-primary--sub u-margin-left-small u-margin-right-small u-margin-bottom-medium u-center-text"> Why job seekers love us </h1>
-                    <h1 className="heading_secondary u-margin-left-medium u-margin-right-medium">TalentPair matches your personality to the best fit job. With just a few simple steps, you can create an account that can be viewed by hiring managers for all companies. All you have to do is wait for HRs to reach out to you! You already have a big chance of landing the job when they do. It is never too late to find a job.</h1>
+                    <h1 className="heading_secondary u-margin-left-medium u-margin-right-huge">TalentPair matches your personality to the best fit job. With just a few simple steps, you can create an account that can be viewed by hiring managers for all companies. All you have to do is wait for HRs to reach out to you! You already have a big chance of landing the job when they do. It is never too late to find a job.</h1>
                     <Link to="/auth/signin" className='section-intro--signlink'>Sign up today! &rarr; </Link>
                 </div>
               </div>
@@ -21,7 +21,7 @@ export default function SectionIntro() {
                 </div>
             </div>
 
-            <div className = "flex-container u-margin-top-medium">
+            <div className = "flex-container u-margin-top-huge">
                 <div className="flex-1-of-3">
                     <img src={imgintro2} alt='Fig intro' className="section-intro--fig-img-2"/>
                 </div>
@@ -29,8 +29,8 @@ export default function SectionIntro() {
                 <div className="flex-2-of-3">
                     <div className="section-intro-text">
                         <h1 className="heading-primary--sub u-margin-left-small u-margin-right-small u-margin-bottom-medium u-center-text"> Matching Making for Tech Startups </h1>
-                        <h1 className="heading_secondary u-margin-left-medium u-margin-right-medium">TalentPair has pool of new CS undergraduates eager to look for jobs. At TalantPair, you can sift through applicants using precise filters, and our algorithm can list the best-matching candidates. This fast, easy process finds you the most qualified candidates regarding skills and personality. Best of all, we are free to browse and only charge a little when you have found your perfect candidate. </h1>
-                        <Link to="/auth/signin" className='section-intro--signlink'>Sign up for a free trial today &rarr; </Link>
+                        <h1 className="heading_secondary u-margin-left-huge u-margin-right-medium">TalentPair has pool of new CS undergraduates eager to look for jobs. At TalantPair, you can sift through applicants using precise filters, and our algorithm can list the best-matching candidates. This fast, easy process finds you the most qualified candidates regarding skills and personality. Best of all, we are free to browse and only charge a little when you have found your perfect candidate. </h1>
+                        <Link to="/auth/signin" className='section-intro--signlink u-margin-left-huge'>Sign up for a free trial today &rarr; </Link>
                     </div>
                 </div>
             </div>
